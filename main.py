9ddd42998c5d1db8368e3f7a48cccc4ce246bb95
Anush <72981395+mister-programmer@users.noreply.github.com>
2021-01-18 15:43:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+s = open(Passlist.txt")
+password = input("Enter the password : ")
+if password in s:
+	print("Weak password")
+
+elif password not in s:
+	print("Strong password")
+
+else:
+	while (True):
+		password = input("please enter a password")
+		continue
+s.close()
